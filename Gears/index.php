bb4520907a7ddb581f6e47347d183869132f17ca
ImpathosCV <chrisv2915@gmail.com>
2024-06-10 11:00:10 -0600

    Script loading added

--- Gears/index.php
+++ Gears/index.php
@@ -1,56 +1,94 @@
 <?php
 
-$json = '../json/%s';
-$gearData = sprintf($json, 'GearData.json');
-$content = null;
+static $json = '../json/FilePath';
+static $images = '../images/FilePath';
+$gearDataPointer = str_replace('FilePath', 'GearData.json', $json);
+$gearScriptsPointer = str_replace('FilePath', 'GearScripts.json', $json);
+$thumbnails = str_replace('FilePath', 'thumbnails/FilePath', $images);
+$content = [];
+$gearName = null;
+$gearThumbnail = null;
 
-//Finding first instance of number
+function unpack_content() {
+    global $content;
+    foreach ($content as $tag) {
+        echo $tag;
+    }
+}
+
+function find_number(string $inputStr, int $count = 1) {
+    preg_match('/\/(\d+)/', $inputStr, $matches);
+    if (count($matches) <= 0) {return 0;}
+    $result = ($count > 1) ? (array_slice($matches, 0, $count)) : $matches[1];
+    return $result;
+}
+
+function unpack_json(string $filePath) {
+    if (count_chars($filePath) <= 0) { echo "Please provide a valid filePath argument"; }
+    $result = file_exists($filePath) ? json_decode(file_get_contents($filePath), true) : "File does not exist";
+    return $result;
+}
+
+function get_image_path(int $gearId) {
+    global $thumbnails;
+    $return = str_replace('FilePath', str_replace('GearId', $gearId, 'GearId.jpg'), $thumbnails);
+    return $return;
+}
+
+function load_script($script = null) {
+    global $content;
+    $scriptType = $script['scriptType'];
+    $scriptName = $script['scriptName'];
+    $scriptSource = $script['scriptSource'];
+
+    $tag = "<pre><h3>$scriptType - $scriptName</h3><code class='language-lua'>$scriptSource</code></pre>";
+    array_push($content, $tag);
+}
+
 $uri = $_SERVER['REQUEST_URI'];
-preg_match('/\/(\d+)/', $uri, $matches);
-$number = $matches[1];
+$number = find_number($uri);
 
 if ($number) {
-    $jsonData = file_get_contents($gearData);
-    $data = json_decode($jsonData, true);
+    $gearData = unpack_json($gearDataPointer);
+    $scriptsData = unpack_json($gearScriptsPointer);
 
-    // Check if the number exists in the JSON data
-    $found = false;
-    foreach ($data as $item) {
-        if ($item['id'] == $number) {
-            $found = true;
-            $name = $item['name'];
-            $scripts = $item['scripts'];
-            $content = "";
-            foreach ($scripts as $script) {
-                $scriptName = $script['scriptName'];
-                $scriptSource = $script['scriptSource'];
-                $scriptType = $script['scriptType'][0];
+    if(!array_key_exists($number, $gearData)) { exit('Error!'); }
 
-                $scriptContent = "<h3>$scriptType - $scriptName</h3><pre style='background-color:#444; color: #fff; padding: 10px; border-radius: 10px'>$scriptSource</pre>";
-            }
-            break;
-        }
-    }
+    $gearId = $number;
+    $item = $gearData[$gearId];
+    $scripts = $scriptsData[$gearId];
+    $gearName = $item['name'];
+    $gearThumbnail = get_image_path($gearId);
 
-    if ($found) {
-        $content = $number;
-    } else {
-        $content = "error";
-        $error = "Unable to locate GearID: $number\nPlease try again later";
+    $headerTag = "<header><img class='gear-thumbnail' src=$gearThumbnail><h1>$gearName</h1></header>";
+    array_push($content, $headerTag);
+
+    foreach ($scripts as $script) {
+        load_script($script);
     }
 } else {
-    $content = "mainPage";
+    exit('Error!');
 }
+
 ?>
+
+<!-- html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gear - <?php echo "$name"; ?></title>
-    <link rel="stylesheet" href="./style.css">
+    <title><?php echo "$gearName"; ?></title>
+    <link rel="stylesheet" href="../styles/itempage.css">
+    <link rel="shortcut icon" href=<?php echo "$gearThumbnail"; ?> />
+    <link rel="stylesheet" href="../styles/highlighter.css"/>
+    <script src="../js/highlight.js"></script>
+    <script src="../js/lua.js"></script>
+    <script> hljs.highlightAll(); </script>
 </head>
 <body>
-    <?php echo $content; ?>
+    <div class='body'>
+        <?php unpack_content(); ?>
+    </div>
 </body>
 </html>
